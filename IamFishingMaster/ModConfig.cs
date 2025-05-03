393f0235a5cc41b436f314ebf30989a0f31e3060
yehaoze <yehaozee@gmail.com>
2025-05-03 22:25:31 +0200

    添加项目文件。

---
+++ IamFishingMaster/ModConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IamFishingMaster
+{
+    public sealed class ModConfig
+    {
+        [Range(1, 100)] // 限制 Increase 在 1 到 100 之间
+        public int staminaMultiplier { get; set; } = 1;
+
+
+        [Range(1, 100)] // 限制 AddingStandards 在 5 到 50 之间
+        public int fishMultiplier { get; set; } = 1;
+    }
+}
---
+++ IamFishingMaster/ModEntry.cs
@@ -0,0 +1,108 @@
+﻿using StardewModdingAPI;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Tools;
+using GenericModConfigMenu;
+
+namespace IamFishingMaster
+{
+
+
+    public class ModEntry : Mod
+    {
+        private ModConfig? Config; //获取配置
+
+
+        public override void Entry(IModHelper helper)
+        {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            int fishMultiplier = this.Config.fishMultiplier; //基础倍率
+            int staminaMultiplier = this.Config.staminaMultiplier; //消耗倍率
+            float realitystaminaMultiplier = (fishMultiplier * staminaMultiplier) * 1f;
+            helper.Events.GameLoop.OneSecondUpdateTicked += OnFishingCheck;
+            helper.Events.Player.InventoryChanged += OnInventoryChanged;
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+
+        }
+
+        private void OnFishingCheck(object sender, OneSecondUpdateTickedEventArgs e)
+        {
+            if (Game1.player.CurrentTool is FishingRod rod && rod.isFishing)
+            {
+                int fishMultiplier = this.Config.fishMultiplier;
+                int staminaMultiplier = this.Config.staminaMultiplier;
+                float realitystaminaMultiplier = (fishMultiplier * staminaMultiplier) * 1f;
+                if (Game1.player.Stamina >= Math.Max(1 * realitystaminaMultiplier, 1))
+                {
+                    Game1.player.Stamina -= Math.Max(1 * realitystaminaMultiplier, 1);
+                    
+
+                }
+            }
+        }
+
+
+
+        private void OnInventoryChanged(object sender, InventoryChangedEventArgs e)
+        {
+            foreach (var item in e.Added)
+            {
+                if (item.Category == StardewValley.Object.FishCategory)
+                {
+                    int fishMultiplier = this.Config.fishMultiplier;
+                    int staminaMultiplier = this.Config.staminaMultiplier;
+                    float realitystaminaMultiplier = (fishMultiplier * staminaMultiplier) * 1f;
+                    if (Game1.player.Stamina >= Math.Max(1 * realitystaminaMultiplier, 1))
+                    {
+                        for (int i = 1; i < fishMultiplier; i++)
+                        {
+                            Game1.player.addItemByMenuIfNecessary(item.getOne());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            // get Generic Mod Config Menu's API (if it's installed)
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // register mod
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => this.Config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(this.Config)
+            );
+
+            // add some config options
+            configMenu.AddSectionTitle(
+                mod: this.ModManifest,
+                text: () => "我是钓鱼大师"
+            );
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "基础倍率",
+                tooltip: () => "影响鱼的数量和消耗的体力, 体力不够的时候收获的数量恢复正常",
+                getValue: () => this.Config.fishMultiplier,
+                setValue: value => this.Config.fishMultiplier = value,
+                min: 1,
+                max: 100
+            );
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "消耗倍率",
+                tooltip: () => "影响消耗的倍率，比如基础倍率=3 消耗倍率=2 3*2=6(消耗6倍的体力)",
+                getValue: () => this.Config.staminaMultiplier,
+                setValue: value => this.Config.staminaMultiplier = value,
+                min: 1,
+                max: 100
+            );
+
+        }
+    }
+}
