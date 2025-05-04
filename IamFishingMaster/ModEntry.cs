5d287d59f256794b16e5a386cb1727b9c9b9b7f2
yehaoze <yehaozee@gmail.com>
2025-05-04 13:10:57 +0200

    1.1.0

--- IamFishingMaster/ModEntry.cs
+++ IamFishingMaster/ModEntry.cs
@@ -5,6 +5,8 @@ using StardewValley;
 using StardewValley.Menus;
 using StardewValley.Tools;
 using GenericModConfigMenu;
+using Microsoft.Xna.Framework.Graphics;
+using static StardewValley.Minigames.CraneGame;
 
 namespace IamFishingMaster
 {
@@ -36,8 +38,24 @@ namespace IamFishingMaster
                 float realitystaminaMultiplier = (fishMultiplier * staminaMultiplier) * 1f;
                 if (Game1.player.Stamina >= Math.Max(1 * realitystaminaMultiplier, 1))
                 {
+                    string text1 = $"-{Math.Max(1 * realitystaminaMultiplier, 1)} 体力";
+                    SpriteFont font = Game1.dialogueFont; // 使用游戏内的字体
+                    Vector2 textSize = font.MeasureString(text1);
+                    float textWidth = 0 - (textSize.X / 10); // 文本宽度（像素）
+                    float textHeight = textSize.Y + 70; // 文本高度（像素）
+
                     Game1.player.Stamina -= Math.Max(1 * realitystaminaMultiplier, 1);
+                    TemporaryAnimatedSprite mySprite = new TemporaryAnimatedSprite(6, Game1.player.Position + new Vector2(textWidth, -textHeight), Color.LimeGreen, 90, false, 1, 0, 0)
+                    {
+                        text = text1,
+                        motion = new Vector2(0, -0.06f),
+                        timeBasedMotion = true,
+                        layerDepth = 1f, // 确保它显示在前景
+                        alphaFade = 0.0005f // 控制透明度逐渐消失的速率
+                    };
+                    Game1.currentLocation.TemporarySprites.Add(mySprite);
                     
+
 
                 }
             }
@@ -93,6 +111,12 @@ namespace IamFishingMaster
                 min: 1,
                 max: 100
             );
+
+            configMenu.SetTitleScreenOnlyForNextOptions(
+                mod: this.ModManifest,
+                titleScreenOnly: true
+            );
+
             configMenu.AddNumberOption(
                 mod: this.ModManifest,
                 name: () => "消耗倍率",
